95772ece0019debf0e97f81594b992314253becd
Kyle Voisard <KVoisard@gmail.com>
2012-11-08 18:13:34 -0500

    GCM Intent Service
    GCMIntentService class and blank methodes & Manifest altered to include
    GCM stuff

--- src/com/example/cryptprototype/GCMIntentService.java
+++ src/com/example/cryptprototype/GCMIntentService.java
@@ -2,4 +2,28 @@ package com.example.cryptprototype;
 
 public class GCMIntentService {
 
+	@Override
+	public void onRegistered(Context context, String regId) {
+		
+	}
+	
+	@Override
+	public void onUnregistered(Context context, String regId) {
+		
+	}
+	
+	@Override
+	public void onMessage(Context context, Intent intent) {
+		
+	}
+	
+	@Override
+	public void onError(Context context, String errorId) {
+		
+	}
+	
+	@Override
+	public void onRecoverableError (Context context, String errorId) {
+		
+	}
 }
